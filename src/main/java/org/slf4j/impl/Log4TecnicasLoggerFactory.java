645e1e9e9773760cbb3448be78673c0da5cacda3
Marcos Rimoldi <marcos.rimoldi@gmail.com>
2014-05-31 17:50:01 -0300

    Carpeta impl agregada.

---
+++ src/main/java/org/slf4j/impl/Log4TecnicasLoggerFactory.java
@@ -0,0 +1,35 @@
+package org.slf4j.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+
+/**
+ * Clase que implementa la la interfaz org.slf4j.ILoggerFactory para retornar
+ * la instancia del Adapter apropiada. 
+ * 
+ * @author Marcos Rimoldi
+ *
+ */
+public class Log4TecnicasLoggerFactory implements ILoggerFactory {
+
+	private Map<String, Log4TecnicasLoggerAdapter> loggerMap;
+	
+	public  Log4TecnicasLoggerFactory() {
+		loggerMap = new HashMap<String, Log4TecnicasLoggerAdapter>();
+	 }
+	
+	public Logger getLogger(String name) {
+		 // protege el acceso concurrente al loggerMap
+		synchronized (loggerMap) {
+            if (!loggerMap.containsKey(name)) {
+                loggerMap.put(name, new Log4TecnicasLoggerAdapter(com.fiuba.logger.domain.Logger.getLogger(name)));
+            }
+            return loggerMap.get(name);
+        }
+	}
+	
+	
+}
--- src/test/java/org/slf4j/AppTest.java
+++ src/test/java/org/slf4j/AppTest.java
@@ -1,38 +1,17 @@
 package org.slf4j;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest { 
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+	@Test
+    public void testApp() {
+		Logger logger = LoggerFactory.getLogger(AppTest.class);
+	    logger.info("Hello World");
+		Assert.assertTrue(true);
     }
 }
